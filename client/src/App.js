d8f7b2187da29764e58828b30cc4fcd72ce37d55
Ashita-Bansal <ashita1304@gmail.com>
2022-11-24 23:57:59 +0530

    forms and styling added

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,31 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import LoginForm from "./components/LoginForm";
 import Logout from "./components/Logout";
+import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import SignUp from "./components/SignUp";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 export default function App() {
    
     return (
-      
       <Router>
+        <Navbar />
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+            </Routes>
+          </div>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm /> } />
           <Route path="/logout" element={<Logout />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/signup" element={<SignUp />} />
-
         </Routes>
       </Router>
     );
